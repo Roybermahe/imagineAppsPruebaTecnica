49ef4b79fbabf55223980f32106824fe86492ea7
Roybermahe <Roybermahe@github.com>
2023-09-20 14:59:36 -0500

    sign up corregido y listo

--- front/src/app/registration/registration.component.ts
+++ front/src/app/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SignUpService } from 'src/services/signup.service';
 
 @Component({
   selector: 'app-registration',
@@ -10,13 +11,29 @@ export class RegistrationComponent {
 
   form: FormGroup
   constructor(
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private signupService: SignUpService
   ) {
     this.form = this.fb.group({
-      nombres: ['',[Validators.required]],
+      nombre: ['',[Validators.required]],
       email: ['',[Validators.email,Validators.required]],
       password: ['',[Validators.required, Validators.minLength(8)]],
       confirmPass: ['',[Validators.required, Validators.minLength(8)]]
     })
+
+    this.form.get('password')?.valueChanges.subscribe(resp => {
+      if(resp !== this.form.get('confirmPass')?.value) {
+        this.form.get('confirmPass')?.patchValue('');
+      }
+    })
+    this.form.get('confirmPass')?.valueChanges.subscribe(resp => {
+      if(resp !== this.form.get('password')?.value) {
+        this.form.get('confirmPass')?.setErrors({ invalidData: true });
+      }
+    })
+  }
+
+  sendSignUp() {
+    this.signupService.signup = this.form.value;
   }
 }
--- front/src/services/signup.service.ts
+++ front/src/services/signup.service.ts
@@ -1,6 +1,44 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-
+import { Router } from "@angular/router";
+import { BehaviorSubject, Subject, debounceTime } from "rxjs";
+import { environment } from '../environment/enviroment';
 @Injectable({
     providedIn: 'root'
 })
-export class SignUpService {}
+export class SignUpService {
+    private backend = environment.backend;
+    private $signup = new Subject();
+    private $message = new BehaviorSubject('');
+  
+    constructor(
+        private http: HttpClient,
+        private router: Router
+    ) {
+        this.$signup.pipe(
+            debounceTime(500)
+        ).subscribe({
+            next: (resp) => {
+                this.http.post(this.backend + '/usuarios', resp).subscribe({
+                    next: (req) => {
+                        if(req.hasOwnProperty('id')){
+                            this.router.navigateByUrl('/login');
+                        }else {
+                            this.$message.next('Ocurrio un error intentando realizar la petición.')
+                        }
+                    },
+                    error: (err) => {
+                    }
+                })
+            }
+        })
+    }
+    
+    get messages() {
+        return this.$message.asObservable();
+    }
+    set signup(data: any) {
+        this.$signup.next(data);
+    }
+
+}
